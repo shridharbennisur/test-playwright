e294d3fc57bba60b1c7d960bebf9c45e9953e2ef
shridharbennisur <shridharmbennisur77@gmail.com>
2025-07-08 20:55:53 +0530

    updated code

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   timeout: 60000,
-  retries: 1,
+  retries: 0,
   use: {
     trace: 'on',
     video: 'on',
@@ -17,15 +17,31 @@ export default defineConfig({
       },
     },
     {
+      name: 'Edge',
+      use: {
+        browserName: 'chromium',
+        viewport: { width: 1280, height: 720 },
+        userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Edge/114.0.1823.67',
+      },
+    },
+    {
       name: 'Chrome',
       use: {
-        channel: 'chrome',
+        browserName: 'chromium',
+        viewport: { width: 1280, height: 720 },
+        userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/114.0.5735.134',
       },
     },
     {
       name: 'Firefox',
       use: {
         browserName: 'firefox',
+      },
+    },
+    {
+      name: 'Pixel7',
+      use: {
+        ...devices['Pixel 7'],
       },
     },
   ],
